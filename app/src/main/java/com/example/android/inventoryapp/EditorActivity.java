ca327a507fddb782246ca77dda37bb98483849dc
valetnt <valetnt@gmail.com>
2017-07-16 00:04:32 +0200

    added editor menu

--- app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -8,6 +8,8 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.FrameLayout;
@@ -26,11 +28,15 @@ public class EditorActivity extends AppCompatActivity
     private TextView mQuantityText;
     private EditText mSupplierEditText;
     private EditText mSupplierEMailEditText;
-    private TextView mImpendingOrdersText;
+    private EditText mImpendingOrdersText;
 
     // Uri of the item we want to edit in edit mode
     private Uri mCurrentUri;
 
+    /**
+     * Implementation of the {@link LoaderManager.LoaderCallbacks} interface.
+     * The cursor loader returns the results of a database query for a specific row.
+     */
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         // Retrieve all fields for the current item
@@ -40,6 +46,7 @@ public class EditorActivity extends AppCompatActivity
                 InventoryEntry.COLUMN_SUPPLIER_NAME, InventoryEntry.COLUMN_SUPPLIER_MAIL,
                 InventoryEntry.COLUMN_IMPENDING_ORDERS};
 
+        // Query database for the Uri mCurrentUri
         return new CursorLoader(this, mCurrentUri, projection, null, null, null);
     }
 
@@ -90,7 +97,14 @@ public class EditorActivity extends AppCompatActivity
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        // Clear all data fields
+        mNameEditText.setText("");
+        mCodeEditText.setText("");
+        mQuantityText.setText("");
+        mPriceEditText.setText("");
+        mSupplierEditText.setText("");
+        mSupplierEMailEditText.setText("");
+        mImpendingOrdersText.setText("");
     }
 
     @Override
@@ -104,12 +118,13 @@ public class EditorActivity extends AppCompatActivity
         mQuantityText = (TextView) findViewById(R.id.item_quantity_in_stock);
         mSupplierEditText = (EditText) findViewById(R.id.item_supplier_name);
         mSupplierEMailEditText = (EditText) findViewById(R.id.item_supplier_email);
-        mImpendingOrdersText = (TextView) findViewById(R.id.number_of_items_ordered);
+        mImpendingOrdersText = (EditText) findViewById(R.id.number_of_items_ordered);
 
         FrameLayout buttonAddPhoto = (FrameLayout) findViewById(R.id.button_add_photo);
         FrameLayout buttonChangePhoto = (FrameLayout) findViewById(R.id.button_change_photo);
         FrameLayout buttonIncreaseQuantity = (FrameLayout) findViewById(R.id.button_increase_quantity);
         FrameLayout buttonDecreaseQuantity = (FrameLayout) findViewById(R.id.button_decrease_quantity);
+        FrameLayout buttonOrder = (FrameLayout) findViewById(R.id.button_order);
 
         buttonIncreaseQuantity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -136,10 +151,14 @@ public class EditorActivity extends AppCompatActivity
             // We are in insert-new-item mode
             getSupportActionBar().setTitle(getString(R.string.editor_activity_title_new_item));
 
+            // Invalidate the options menu, so the "Delete" menu option can be hidden.
+            // (It doesn't make sense to delete an item that hasn't been created yet.)
+            invalidateOptionsMenu();
+
             mQuantityText.setText("0");
-            mImpendingOrdersText.setText("0");
             buttonChangePhoto.setVisibility(View.GONE);
             buttonAddPhoto.setVisibility(View.VISIBLE);
+            buttonOrder.setVisibility(View.GONE);
 
         } else {
             // We are in edit-existing-item mode
@@ -147,10 +166,30 @@ public class EditorActivity extends AppCompatActivity
 
             buttonChangePhoto.setVisibility(View.VISIBLE);
             buttonAddPhoto.setVisibility(View.GONE);
+            buttonOrder.setVisibility(View.VISIBLE);
 
             // Retrieve item data via cursor loader
             LoaderManager loaderManager = getSupportLoaderManager();
             loaderManager.initLoader(EDITOR_LOADER, null, this);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new item, hide the "Delete" menu item.
+        if (mCurrentUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
+        return true;
     }
 }
